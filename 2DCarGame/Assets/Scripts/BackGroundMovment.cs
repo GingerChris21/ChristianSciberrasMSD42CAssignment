67683d0374ec795d8a3f35a8e94fe91d626aa064
GingerChris21 <christian.sciberras.e21827@mcast.edu.mt>
2021-01-06 17:12:15 +0100

    Continuing Task 1 (e, f, g and l)
    Continued work on Task 1 on ViewPortToWorldPoint to create a border around the Car game's camera, set the background of the game to scrolling, added new subfolders for the various types of files (such as c# files) and made the player car move in x-axis.

---
+++ 2DCarGame/Assets/Scripts/BackGroundMovment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMovment : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 3.5f;
+    
+    Material gameMaterial;
+
+    Vector2 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameMaterial = GetComponent<Renderer>().material;
+
+        offset = new Vector2(0f, backgroundScrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
---
+++ 2DCarGame/Assets/Scripts/Car_Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Player : MonoBehaviour
+{
+
+    [SerializeField] float padding = 0.7f;
+    [SerializeField] float moveSpeed = 5f;
+    float xMin, xMax, yMin, yMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move_car();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+
+        Camera gameCamera = Camera.main;
+
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    private void Move_car()
+    {
+        //deltaX is updated with the movment in the x-axis (left and right)
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+
+        //newXPos = current x-pos of Player
+        //+ diffrence in X by keyboard Input
+        var newXPos = transform.position.x + deltaX;
+        //clamps the newXPos between xMin and xMax
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+        this.transform.position = new Vector2(newXPos, transform.position.y);
+
+    }
+
+}
