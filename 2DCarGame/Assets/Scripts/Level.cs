06f959953895cfb5917161ca7f9b2ec9b3c408a1
GingerChris21 <christian.sciberras.e21827@mcast.edu.mt>
2021-01-12 11:05:04 +0100

    Start of task 2
    Implemented Task 2 question a in regards to Level script

---
+++ 2DCarGame/Assets/Scripts/Level.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] float Extend = 3f;
+
+    IEnumerator WaitingonLoad()
+    {
+        yield return new WaitForSeconds(Extend);
+        SceneManager.LoadScene("Game Over");
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Car_Game");
+    }
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitingonLoad());
+    }
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
